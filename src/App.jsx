1e90b4700e74e47e6928c22d89e2e8d131422053
will <willwind0414@gmail.com>
2025-03-08 23:43:46 +0800

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ProjectsUx from './pages/ProjectsUx.jsx';
 
 const App = () => {
   return (
-    <Router> {/* 這裡設置了 basename */}
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projectsweb/:id" element={<ProjectsWeb />} />
