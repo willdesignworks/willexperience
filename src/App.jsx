15e3a56e2a3e62ab6682c6ee947d45aaed6ded6f
will <willwind0414@gmail.com>
2025-03-09 17:47:38 +0800

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // 正確引入 Routes 和 Route
 import Home from './pages/Home.jsx';
 import ProjectsWeb from './pages/ProjectsWeb.jsx';
 import ProjectsSocialMedia from './pages/ProjectsSocialMedia.jsx';
@@ -8,17 +8,15 @@ import ProjectsUx from './pages/ProjectsUx.jsx';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projectsweb/:id" element={<ProjectsWeb />} />
-        <Route path="/projectssocialmedia/:id" element={<ProjectsSocialMedia />} />
-        <Route path="/projectsedm/:id" element={<ProjectsEdm />} />
-        <Route path="/projectapp/:id" element={<ProjectApp />} />
-        <Route path="/projectsux/:id" element={<ProjectsUx />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projectsweb/:id" element={<ProjectsWeb />} />
+      <Route path="/projectssocialmedia/:id" element={<ProjectsSocialMedia />} />
+      <Route path="/projectsedm/:id" element={<ProjectsEdm />} />
+      <Route path="/projectapp/:id" element={<ProjectApp />} />
+      <Route path="/projectsux/:id" element={<ProjectsUx />} />
+    </Routes>
   );
 };
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // 正確引入 React 18
 import './index.css';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 正確引入 HashRouter
 
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <HashRouter> {/* 在 index.js 中包裹 HashRouter */}
     <App />
-  </Router>,
-  document.getElementById('root')
+  </HashRouter>
 );
