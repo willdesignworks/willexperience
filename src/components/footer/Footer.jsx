652e00580e9889a6c1bc8c8291e0ada1728e0c80
will <willwind0414@gmail.com>
2025-03-10 00:46:41 +0800

    git init

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
-//import { BsLinkedin } from 'react-icons/bs';
-//import { FaGithub } from 'react-icons/fa';
-//import { FaAngellist } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 import './footer.css';
 
+// 滑动到指定位置的函数
+const ScrollToSection = (id) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
 const Footer = () => {
-  
   let getYear = () => {
     let currentYear = new Date().getFullYear();
     return currentYear;
   };
-  
+
   return (
     <footer>
-      <a href="#home" className="footer__logo">Web DESIGNER</a>
+      <Link to="#" className="footer__logo" onClick={() => ScrollToSection('home')}>Web DESIGNER</Link>
       <ul className="permalinks">
-        <li><a href="#home">Home<br/><span className="foot-translate">首頁</span></a></li>
-        <li><a href="#about">About<br/><span className="foot-translate">簡介</span></a></li>
-        <li><a href="#experience">Skills<br/><span className="foot-translate">技能</span></a></li>
-        <li><a href="#timeline">Experience<br/><span className="foot-translate">經歷</span></a></li>
-        <li><a href="#portfolio">Projects<br/><span className="foot-translate">專案</span></a></li>
-        {/*<li><a href="#testimonials">Testimonials</a></li>*/}
-        {/*<<li><a href="#contact">Contact</a></li>*/}
+        <li>
+          <Link to="#" onClick={() => ScrollToSection('home')}>Home<br /><span className="foot-translate">首頁</span></Link>
+        </li>
+        <li>
+          <Link to="#" onClick={() => ScrollToSection('about')}>About<br /><span className="foot-translate">簡介</span></Link>
+        </li>
+        <li>
+          <Link to="#" onClick={() => ScrollToSection('experience')}>Skills<br /><span className="foot-translate">技能</span></Link>
+        </li>
+        <li>
+          <Link to="#" onClick={() => ScrollToSection('timeline')}>Experience<br /><span className="foot-translate">經歷</span></Link>
+        </li>
+        <li>
+          <Link to="#" onClick={() => ScrollToSection('portfolio')}>Projects<br /><span className="foot-translate">專案</span></Link>
+        </li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
+        {/* <li><a href="#contact">Contact</a></li> */}
       </ul>
-      {/*<div className="footer__socials">
-      <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="https://angel.co/u/meri-gogichashvili" target="_blank" rel="noreferrer" ><FaAngellist /></a>
-      </div>*/}
       <div className="footer__copyright">
         <small>&copy; Will {getYear()}. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,23 +1,83 @@
 import React, { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
-//import { BiMessageSquareDetail } from 'react-icons/bi'; //contact
-
-
 import './topbar.css';
 
 const Topbar = () => {
   const [activeNav, setActiveNav] = useState('#home');
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav>
-      <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''} alt="首頁"><AiOutlineHome /></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} alt="簡介"><AiOutlineUser /></a>
-      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} alt="技能"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.32943 3.27158C6.56252 2.8332 7.9923 3.10749 8.97927 4.09446C10.1002 5.21537 10.3019 6.90741 9.5843 8.23385L20.293 18.9437L18.8788 20.3579L8.16982 9.64875C6.84325 10.3669 5.15069 10.1654 4.02952 9.04421C3.04227 8.05696 2.7681 6.62665 3.20701 5.39332L5.44373 7.63C6.02952 8.21578 6.97927 8.21578 7.56505 7.63C8.15084 7.04421 8.15084 6.09446 7.56505 5.50868L5.32943 3.27158ZM15.6968 5.15512L18.8788 3.38736L20.293 4.80157L18.5252 7.98355L16.7574 8.3371L14.6361 10.4584L13.2219 9.04421L15.3432 6.92289L15.6968 5.15512ZM8.97927 13.2868L10.3935 14.7011L5.09018 20.0044C4.69966 20.3949 4.06649 20.3949 3.67597 20.0044C3.31334 19.6417 3.28744 19.0699 3.59826 18.6774L3.67597 18.5902L8.97927 13.2868Z"></path></svg></a>
-      <a href="#timeline" onClick={()=> setActiveNav('#timeline')} className={activeNav === '#timeline' ? 'active' : ''} alt="經歷"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 6.99999C16.4183 6.99999 20 10.5817 20 15C20 19.4183 16.4183 23 12 23C7.58172 23 4 19.4183 4 15C4 10.5817 7.58172 6.99999 12 6.99999ZM12 8.99999C8.68629 8.99999 6 11.6863 6 15C6 18.3137 8.68629 21 12 21C15.3137 21 18 18.3137 18 15C18 11.6863 15.3137 8.99999 12 8.99999ZM12 10.5L13.3225 13.1797L16.2798 13.6094L14.1399 15.6953L14.645 18.6406L12 17.25L9.35497 18.6406L9.86012 15.6953L7.72025 13.6094L10.6775 13.1797L12 10.5ZM18 1.99999V4.99999L16.6366 6.13755C15.5305 5.5577 14.3025 5.17884 13.0011 5.04948L13 1.99899L18 1.99999ZM11 1.99899L10.9997 5.04939C9.6984 5.17863 8.47046 5.55735 7.36441 6.13703L6 4.99999V1.99999L11 1.99899Z"></path></svg></a>
-      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} alt="專案"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8 4C8 5.10457 7.10457 6 6 6 4.89543 6 4 5.10457 4 4 4 2.89543 4.89543 2 6 2 7.10457 2 8 2.89543 8 4ZM5 16V22H3V10C3 8.34315 4.34315 7 6 7 6.82059 7 7.56423 7.32946 8.10585 7.86333L10.4803 10.1057 12.7931 7.79289 14.2073 9.20711 10.5201 12.8943 9 11.4587V22H7V16H5ZM6 9C5.44772 9 5 9.44772 5 10V14H7V10C7 9.44772 6.55228 9 6 9ZM19 5H10V3H20C20.5523 3 21 3.44772 21 4V15C21 15.5523 20.5523 16 20 16H16.5758L19.3993 22H17.1889L14.3654 16H10V14H19V5Z"></path></svg></a>
-      {/*<a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>*/}
+      <a
+        href="#home"
+        onClick={(e) => { 
+          e.preventDefault();
+          setActiveNav('#home');
+          scrollToSection('home');
+        }}
+        className={activeNav === '#home' ? 'active' : ''}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={(e) => {
+          e.preventDefault();
+          setActiveNav('#about');
+          scrollToSection('about');
+        }}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={(e) => {
+          e.preventDefault();
+          setActiveNav('#experience');
+          scrollToSection('experience');
+        }}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M5.32943 3.27158C6.56252 2.8332 7.9923 3.10749 8.97927 4.09446C10.1002 5.21537 10.3019 6.90741 9.5843 8.23385L20.293 18.9437L18.8788 20.3579L8.16982 9.64875C6.84325 10.3669 5.15069 10.1654 4.02952 9.04421C3.04227 8.05696 2.7681 6.62665 3.20701 5.39332L5.44373 7.63C6.02952 8.21578 6.97927 8.21578 7.56505 7.63C8.15084 7.04421 8.15084 6.09446 7.56505 5.50868L5.32943 3.27158ZM15.6968 5.15512L18.8788 3.38736L20.293 4.80157L18.5252 7.98355L16.7574 8.3371L14.6361 10.4584L13.2219 9.04421L15.3432 6.92289L15.6968 5.15512ZM8.97927 13.2868L10.3935 14.7011L5.09018 20.0044C4.69966 20.3949 4.06649 20.3949 3.67597 20.0044C3.31334 19.6417 3.28744 19.0699 3.59826 18.6774L3.67597 18.5902L8.97927 13.2868Z"></path>
+        </svg>
+      </a>
+      <a
+        href="#timeline"
+        onClick={(e) => {
+          e.preventDefault();
+          setActiveNav('#timeline');
+          scrollToSection('timeline');
+        }}
+        className={activeNav === '#timeline' ? 'active' : ''}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M12 6.99999C16.4183 6.99999 20 10.5817 20 15C20 19.4183 16.4183 23 12 23C7.58172 23 4 19.4183 4 15C4 10.5817 7.58172 6.99999 12 6.99999ZM12 8.99999C8.68629 8.99999 6 11.6863 6 15C6 18.3137 8.68629 21 12 21C15.3137 21 18 18.3137 18 15C18 11.6863 15.3137 8.99999 12 8.99999ZM12 10.5L13.3225 13.1797L16.2798 13.6094L14.1399 15.6953L14.645 18.6406L12 17.25L9.35497 18.6406L9.86012 15.6953L7.72025 13.6094L10.6775 13.1797L12 10.5ZM18 1.99999V4.99999L16.6366 6.13755C15.5305 5.5577 14.3025 5.17884 13.0011 5.04948L13 1.99899L18 1.99999ZM11 1.99899L10.9997 5.04939C9.6984 5.17863 8.47046 5.55735 7.36441 6.13703L6 4.99999V1.99999L11 1.99899Z"></path>
+        </svg>
+      </a>
+      <a
+        href="#portfolio"
+        onClick={(e) => {
+          e.preventDefault();
+          setActiveNav('#portfolio');
+          scrollToSection('portfolio');
+        }}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M8 4C8 5.10457 7.10457 6 6 6 4.89543 6 4 5.10457 4 4 4 2.89543 4.89543 2 6 2 7.10457 2 8 2.89543 8 4ZM5 16V22H3V10C3 8.34315 4.34315 7 6 7 6.82059 7 7.56423 7.32946 8.10585 7.86333L10.4803 10.1057 12.7931 7.79289 14.2073 9.20711 10.5201 12.8943 9 11.4587V22H7V16H5ZM6 9C5.44772 9 5 9.44772 5 10V14H7V10C7 9.44772 6.55228 9 6 9ZM19 5H10V3H20C20.5523 3 21 3.44772 21 4V15C21 15.5523 20.5523 16 20 16H16.5758L19.3993 22H17.1889L14.3654 16H10V14H19V5Z"></path>
+        </svg>
+      </a>
     </nav>
-  )
-}
+  );
+};
 
 export default Topbar;
